91e3553341337a8a9951afdf5caf16d0f2fb9815
Jonas Kluger <39433953+jonaskluger@users.noreply.github.com>
2023-07-02 14:03:46 +0200

    Update create_filmstrip.py

--- scripts/custom/examples/create_filmstrip.py
+++ scripts/custom/examples/create_filmstrip.py
@@ -1,4 +1,4 @@
-"""
+f"""
            __                   __                          __
       ____/ /___ ______   ___  / /__  ____ ___  ___  ____  / /_
      / __  / __ `/ ___/  / _ \/ / _ \/ __ `__ \/ _ \/ __ \/ __/
@@ -189,7 +189,7 @@ def main(*args):
         print('No sequence or movie file - will not render filmstrip')
         return False
 
-    paths_frames = []  # temporary file pathes
+    paths_frames = []  # temporary file paths
 
     command = [EXECUTABLE_FFMPEG, '-y', '-vsync', '0']
     streams = ''
