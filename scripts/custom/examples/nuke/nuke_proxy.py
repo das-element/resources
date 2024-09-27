0bab0c682cbee68f5f25d15d354d99a201bec7f6
Jonas Kluger <39433953+jonaskluger@users.noreply.github.com>
2024-09-27 11:33:08 +0200

    Update nuke_proxy.py

--- scripts/custom/examples/nuke/nuke_proxy.py
+++ scripts/custom/examples/nuke/nuke_proxy.py
@@ -84,7 +84,7 @@ if not os.path.exists(path_script_directory):
     os.makedirs(path_script_directory)
 
 # save the job nuke script
-nuke.scriptSaveAs(str(path_script_job))
+nuke.scriptSaveAs(str(path_script_job), overwrite=True)
 
 # render the job
 nuke.execute(node_write, frame_first, frame_last, continueOnError=False)
