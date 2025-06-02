5b35c9d79cb3db12e9806eb76d60c658d7568e6a
Jonas Kluger <jonas@das-element.com>
2025-06-02 11:02:28 +0200

    add new hook file: pre gallery drag

---
+++ scripts/hooks/examples/gallery/pre_gallery_drag.js
@@ -0,0 +1,53 @@
+/*
+           __                   __                          __
+      ____/ /___ ______   ___  / /__  ____ ___  ___  ____  / /_
+     / __  / __ `/ ___/  / _ \/ / _ \/ __ `__ \/ _ \/ __ \/ __/
+    / /_/ / /_/ (__  )  /  __/ /  __/ / / / / /  __/ / / / /_
+    \__,_/\__,_/____/   \___/_/\___/_/ /_/ /_/\___/_/ /_/\__/
+
+
+Script that gets run before dragging elements from the gallery view
+The output of this script will be the dragged data in a JSON format
+
+Example data for items:
+
+[{
+    'uuuid': '01859093927b96d3a5131f9cd42d8ed7',
+    'uuuid_short': '96d3a513',
+    'name': 'awesome_element_00044',
+    'category': 'fire',
+    'category_id': 'Q3196',
+    'channel': 3,
+    'height': 1080,
+    'width': 192,
+    'pixel_aspect': '1',
+    'frame_first': 1001,
+    'frame_last': 1049,
+    'frame_count': 50,
+    'frame_rate': '25',
+    'created_at': '2021-10-01 09:26',
+    'media_type': 'movie',
+    'number': '00044',
+    'colorspace': 'sRGB',
+    'colorspace_source': 'sRGB',
+    'popularity': 23,
+    'tags': 'fire, flame, some other tag',
+    'path': '/path/to/server/awesome_element_0044/main_1920x1080_srgb/awesome_element_0044.mov',
+    'path_thumbnail': '/path/to/server/awesome_element_0044/thumb_960x540/awesome_element_0044.jpg',
+    'path_proxy': '/path/to/server/awesome_element_0044/proxy_1920x1080/awesome_element_0044.mov',
+    'path_filmstrip': '/path/to/server/awesome_element_0044/filmstrip_11520x270/awesome_element_0044.jpg'
+    'path_source': '/server/path/to/source_file.mov',
+}]
+*/
+
+export default function main(items) {
+  let result = [];
+
+  // iterate over selected and dragged items and run custom logic
+  items.forEach((item) => {
+    const path_main = item.path;
+    result.push(path_main);
+  });
+
+  return result;
+}
